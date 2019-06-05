24c1b425fece3971b3719bfb3a1c48a010f51e09
Sonny Li <sl4142@columbia.edu>
2019-06-05 14:05:41 -0400

    upload NextYear

---
+++ 9-graphics/NextYear.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Greetings {
+
+    public static void main (String[] args) {
+
+        String name = JOptionPane.showInputDialog("What's your name?");
+
+        String input = JOptionPane.showInputDialog("How old are you?");
+        int age = Integer.parseInt(input);
+
+        System.out.print("Hello, " + name + ". ");
+        System.out.println("Next year, you'll be " + (age + 1));
+
+        System.exit(0);
+        
+    }
+
+}
